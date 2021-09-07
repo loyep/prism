da0224cdcdf5c88feafefea8458ccefbdb1d576f
Zack <mwl@live.com>
2021-09-07 23:59:52 +0800

    chore: update

--- web/components/layout/App.vue
+++ web/components/layout/App.vue
@@ -1,10 +1,15 @@
 <template>
   <router-view />
+  <!-- <Checkbox/> -->
+  <!-- <Button>test</Button> -->
 </template>
 
 <script lang="ts">
+// import { Checkbox } from 'ant-design-vue'
 // 在这里可以进行一些全局组件的注册逻辑
 export default {
-
+  components: {
+    // Checkbox,
+  },
 }
 </script>
