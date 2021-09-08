06cccb989d32bd07a679fef037d38becab51663f
Zack <mwl@live.com>
2021-09-08 20:52:06 +0800

    feat: 更新

--- web/@types/global.d.ts
+++ web/@types/global.d.ts
@@ -1,11 +1,15 @@
 import { IWindow } from 'ssr-types'
+import { App } from 'vue'
+import { Router } from 'vue-router'
 
 declare global {
   interface Window {
     __USE_SSR__?: IWindow['__USE_SSR__']
     __INITIAL_DATA__?: IWindow['__INITIAL_DATA__']
     STORE_CONTEXT?: IWindow['STORE_CONTEXT']
-    globalData?: any
+    __VUE_APP__: App
+    __VUE_ROUTER__: Router
+    __USE_VITE__?: boolean
   }
   const __isBrowser__: Boolean
 }
