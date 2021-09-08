062ce2f93199a20ab6a8f6866bce4b201a0262c3
Zack <mwl@live.com>
2021-09-08 19:09:25 +0800

    feat: 更新

--- web/components/layout/index.vue
+++ web/components/layout/index.vue
@@ -1,32 +1,30 @@
-<template>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="theme-color" content="#000000" />
-    <title>Serverless Side Render for Vue3</title>
-    <slot name="viteClient" />
-    <slot name="customeHeadScript" />
-    <slot name="cssInject" />
-  </head>
-  <body>
-    <div id="app">
-      <slot name="children" />
-    </div>
-    <slot name="initialData" />
-    <slot name="customeFooterScript" />
-    <slot name="jsInject" />
-  </body>
-</html>
-</template>
+
+
 <script lang="tsx">
-
 import { defineComponent, } from 'vue'
-
 export default defineComponent({
   setup(props, ctx) {
-    console.log(props)
-    console.log(ctx.slots)
-  }
+    return () => {
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="theme-color" content="#000000" />
+          <title>Serverless Side Render for Vue3</title>
+          <slot name="viteClient" />
+          <slot name="customeHeadScript" />
+          <slot name="cssInject" />
+        </head>
+        <body>
+          <div id="app">
+            <slot name="children" />
+          </div>
+          <slot name="initialData" />
+          <slot name="customeFooterScript" />
+          <slot name="jsInject" />
+        </body>
+      </html>
+    }
+  },
 })
 </script>
