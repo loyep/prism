b7833f30e2977f219ae38c284ca839ba39bc2791
Zack <mwl@live.com>
2021-09-08 23:26:05 +0800

    chore: update

--- config.js
+++ config.js
@@ -23,9 +23,11 @@ const config = {
   },
   chainClientConfig(config) {
     config.optimization.get('splitChunks').chunks = 'initial'
-    // config
-    //   .plugin('webpack-bundle-analyzer')
-    //   .use(new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)({ analyzerPort: 9999 }))
+    if (process.env.BUNDLE_ANALYZE === 'true') {
+      config
+        .plugin('webpack-bundle-analyzer')
+        .use(new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)({ analyzerPort: 9999 }))
+    }
     config.plugin('antd-dayjs-webpack-plugin').use(new (require('antd-dayjs-webpack-plugin'))())
     return config.toConfig()
   },
