a3a00146aef3de9222497046cacbe613d4edfb1f
Zack <mwl@live.com>
2021-09-10 22:54:20 +0800

    chore: update

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,5 +1,10 @@
+// const packageInfo = require('../package.json')
+import fs from 'fs'
+import path from 'path'
+
 export function main() {
-  console.log('11')
+  const bundleFilePath = path.join(__dirname, '../public/BUNDLE_ID')
+  fs.writeFileSync(bundleFilePath, String(Date.now()), 'utf8')
 }
 
 main()
--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,16 @@
+import path from 'path'
+import fs from 'fs'
+
+function getBundleId() {
+  const bundleFilePath = path.join(process.cwd(), './public/BUNDLE_ID')
+  return fs.existsSync(bundleFilePath) ? fs.readFileSync(bundleFilePath, 'utf8') : ''
+}
+
 function app() {
   return {
     name: 'kova',
+
+    bundleId: getBundleId(),
   }
 }
 
