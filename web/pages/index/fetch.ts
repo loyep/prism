e91a1bbaace158791c2e6d6aca6a22090beea8e1
Zack <mwl@live.com>
2021-09-10 22:58:50 +0800

    chore: update

--- web/pages/index/fetch.ts
+++ web/pages/index/fetch.ts
@@ -9,6 +9,6 @@ interface Params {
 
 export default async ({ store, router }: Params, ctx?: ISSRContext<any>) => {
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
-  const data = __isBrowser__ ? await (await window.fetch('/api/index')).json() : ctx.pageProps
+  const data = __isBrowser__ ? await (await window.fetch('/api/articles')).json() : ctx.pageProps
   await store.dispatch('home/initData', { payload: data })
 }
