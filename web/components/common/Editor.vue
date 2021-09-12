17b2416002a123a4888fb79ca5c56d0b3672307d
Zack <mwl@live.com>
2021-09-12 14:58:45 +0800

    chore: update

--- web/components/common/Editor.vue
+++ web/components/common/Editor.vue
@@ -1,18 +1,31 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
-// import("vditor/index.css")
+import { Spin } from 'ant-design-vue'
 const editor = ref()
+const loadig = ref(false)
 
 onMounted(async () => {
+    loadig.value = true
     import('vditor').then((VditorModule) => {
         const Vditor = VditorModule.default
-        editor.value = new Vditor('vditor', {})
+        editor.value = new Vditor('vditor', {
+            cache: {
+                enable: false,
+            },
+            after: () => {
+                // editor.value = vditor
+                console.log('Vditor11')
+                loadig.value = false
+            }
+        })
     })
 })
 </script>
 
 <template>
-    <div id="vditor"></div>
+    <Spin :spinning="loadig">
+        <div id="vditor"></div>
+    </Spin>
 </template>
 
 <style >
