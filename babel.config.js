5ec9ae8b48f05fae124194407f9617a6fd9dd2d5
Zack <mwl@live.com>
2021-09-14 15:07:44 +0800

    feat: 更新

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,9 @@ module.exports = function (api) {
   api.cache(true)
   return {
     plugins: [
-      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      // ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      // ['@babel/plugin-proposal-class-properties', { loose: true }],
+      // ['@babel/plugin-proposal-private-methods', { loose: true }],
     ],
   }
 }
