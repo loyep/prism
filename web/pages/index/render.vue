0b71a0300e8cecbc439bc839cc259bd0f178e905
Zack <mwl@live.com>
2021-09-14 13:13:31 +0800

    feat: 更新

--- web/pages/index/render.vue
+++ web/pages/index/render.vue
@@ -7,10 +7,11 @@
     <ClientOnly>test1111</ClientOnly>
 
     <RouterLink to="/register">Register</RouterLink>
+    <TestDemo />
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="tsx" setup>
 import ArticleItem from '@/components/article/item.vue'
 import { computed, onMounted } from 'vue';
 import { useStore } from 'vuex'
@@ -19,6 +20,10 @@ import { RouterLink } from 'vue-router'
 
 const store = useStore()
 const articles = computed(() => store.state.home.articles)
+
+const TestDemo = () => {
+  return <div>基于Render方法渲染的标签</div>
+}
 
 onMounted(() => {
   // console.log('1')
